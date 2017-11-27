fa05d0df8675980a8a7eae9f632ee0d8ecdcf164
colekins <colekins@gmail.com>
2017-11-27 11:31:25 -0500

    Functioning 'Delete'
    - 'Delete' button on individual handlebars components
    -  Successful AJAX requests to remove books
    - UI updates dynamically
    
    - Need to add edit functionality
      - "up next" radio field
      - notes field
      - pre-populate form fields with book info

--- assets/scripts/books/events.js
+++ assets/scripts/books/events.js
@@ -14,9 +14,21 @@ const onAddBook = function (event) {
     .catch(ui.addBookFail)
 }
 
+const onDeleteBook = function (event) {
+  event.preventDefault()
+  const button = event.target
+  const panel = button.parentElement.parentElement.parentElement
+  const data = $(button).attr('data-id')
+  console.log(data)
+  api.destroy(data)
+    .then(ui.deleteSuccess)
+    .then(panel.remove())
+    .catch(ui.deleteFail)
+}
+
 const addHandlers = () => {
   $('#add-book').on('submit', onAddBook)
-  // $('#content').on('click', '#delete-book', onDeleteBook)
+  $('#content').on('click', '#delete-book', onDeleteBook)
   // $('#content').on('click', '#edit-book', openEdit)
   // $('#edit-book').on('submit', onEditBook)
 }
--- assets/scripts/books/ui.js
+++ assets/scripts/books/ui.js
@@ -5,7 +5,6 @@ const addBookSuccess = function (data) {
   const bookHtml = bookTemplate({ books: data })
   store.newBook = data.book
   store.books.push(data.book)
-  console.log('new book is ', data)
   $('.content').append(bookHtml)
   $('#message').text(data.book.title + ' has been added to your collection!')
 }
