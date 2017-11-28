91b21481dcbee73a5a4083df155b4fd03353c741
colekins <colekins@gmail.com>
2017-11-28 10:12:36 -0500

    Book cover on add-book
    - Book cover now populates after user adds a book to their collection

--- assets/scripts/books/ui.js
+++ assets/scripts/books/ui.js
@@ -1,8 +1,23 @@
 const store = require('../store')
 const bookTemplate = require('../templates/new-book.handlebars')
+const books = require('google-books-search')
 
 const showClearSearch = function () {
   $('.search').show()
+}
+
+const setCover = function (id, img) {
+  const panel = document.getElementsByClassName(id)
+  const imageHtml = ("<img class='cover' src='" + img + "'>")
+  $(panel).find('p').append(imageHtml)
+}
+
+const google = function (title) {
+  books.search(title, function (error, results) {
+    store.newBook.image = results[0].thumbnail
+    const err = error
+    setCover(store.newBook.id, store.newBook.image)
+  })
 }
 
 const addBookSuccess = function (data) {
@@ -11,12 +26,18 @@ const addBookSuccess = function (data) {
   store.books.push(data.book)
   $('.content').append(bookHtml)
   $('#message').text(data.book.title + ' has been added to your collection!')
+  google(store.newBook.title)
 }
 
 const editBookSuccess = function (data) {
   store.newBook = data
   $('#message').text(data.book.title + ' by ' + data.book.author + ' has been updated.')
   $('#editModal').modal('hide')
+}
+
+const deleteSuccess = function (data) {
+  // console.log(data)
+  $('#message').text('Book deleted.')
 }
 
 const searchSuccess = function () {
@@ -34,5 +55,6 @@ module.exports = {
   addBookSuccess,
   editBookSuccess,
   searchSuccess,
-  clearSearch
+  clearSearch,
+  deleteSuccess
 }
