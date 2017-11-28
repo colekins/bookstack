35d73826085e0545562a88370844da82f7026b7f
colekins <colekins@gmail.com>
2017-11-28 12:02:20 -0500

    Adds font updates
     - Updates font across entire UI

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,7 +7,7 @@ const bookEvents = require('./books/events')
 
 $(() => {
   authEvents.addHandlers()
-  // authEvents.getCartoon() commenting out for now
+  authEvents.getCartoon()
   bookEvents.addHandlers()
   setAPIOrigin(location, config)
 })
