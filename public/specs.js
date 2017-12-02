2a2a3bd47ebb36943db3b67b3f6184ff48b3ced5
colekins <colekins@gmail.com>
2017-12-02 12:13:19 -0500

    deploy task

---
+++ public/specs.js
@@ -0,0 +1,57 @@
+webpackJsonp([1],{
+
+/***/ 90:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+// Load all specs so webpack can find them. Think of this as an automatic
+// manifest for bundling specs.
+
+var req = __webpack_require__(91);
+req.keys().forEach(req);
+
+/***/ }),
+
+/***/ 91:
+/***/ (function(module, exports, __webpack_require__) {
+
+var map = {
+	"./example.spec.js": 92
+};
+function webpackContext(req) {
+	return __webpack_require__(webpackContextResolve(req));
+};
+function webpackContextResolve(req) {
+	var id = map[req];
+	if(!(id + 1)) // check for number or string
+		throw new Error("Cannot find module '" + req + "'.");
+	return id;
+};
+webpackContext.keys = function webpackContextKeys() {
+	return Object.keys(map);
+};
+webpackContext.resolve = webpackContextResolve;
+module.exports = webpackContext;
+webpackContext.id = 91;
+
+/***/ }),
+
+/***/ 92:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+describe('Example DOM test', function () {
+  var myElement = document.getElementById('myDiv');
+
+  it('has the right text', function () {
+    myElement.innerHTML.should.equal('Hello World!');
+  });
+});
+
+/***/ })
+
+},[90]);
